5d7f745507623cc811c07ebdd6a827339a7ec3f0
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-07-10 16:21:20 +0300

    Boilerplate

---
+++ src/Experience/Camera.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three'
+import Experience from "./Experience";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+export default class Camera {
+    constructor() {
+        this.experience = new Experience()
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.canvas = this.experience.canvas
+        this.setInstance()
+        this.OrbitControls()
+    }
+    setInstance(){
+        this.instance = new THREE.PerspectiveCamera(35,this.sizes.width / this.sizes.height,0.1,100)
+        this.instance.position.set(6,4,8)
+        this.scene.add(this.instance)
+    }
+    OrbitControls(){
+        this.controls = new OrbitControls(this.instance,this.canvas)
+        this.controls.enableDamping = true
+    }
+    resize(){
+        this.instance.aspect= this.sizes.width / this.sizes.height
+        this.instance.updateProjectionMatrix()
+    }
+    update(){
+        this.controls.update()
+    }
+    }
+  
---
+++ src/Experience/Utils/Sizes.js
@@ -0,0 +1,22 @@
+import EventEmitter from "./EventEmitter"
+
+export default class Sizes extends EventEmitter{
+    constructor(){
+        super()
+        
+        // Setup
+        this.height = window.innerHeight
+        this.width = window.innerWidth
+        this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+        
+        
+        // Resize event
+        window.addEventListener('resize', () => {
+            this.height = window.innerHeight
+            this.width = window.innerWidth
+            this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+            this.trigger('resize')
+        })
+    }
+}
